3ad7b5a54cb648af6f5969f176ae7f449f3062f4
midiam1 <admhimmeros@gmail.com>
2021-12-23 16:17:34 -0400

    Actualización

--- readme.php
+++ readme.php
@@ -1,5 +1,5 @@
 <img src="https://github.com/midiam1/Webmin/blob/main/img/webmin.png"/>
-<hr/>
+# webmin<p><hr>
  Instalación de <strong><a href="https://www.webmin.com/" title="Ir a ..." target="_blank">webmin</a></strong> en Ubuntu.
 <hr/>
 

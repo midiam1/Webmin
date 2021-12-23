fb3680cc0a67b1c284a2db263f40eff0e36c9c15
midiam1 <admhimmeros@gmail.com>
2021-12-23 16:21:02 -0400

    Hoja de estilos
    Para que quede bien bonito, primero muerto que mal arreglado .

--- readme.php
+++ readme.php
@@ -1,6 +1,7 @@
+<link href="css/estilos.css" rel="stylesheet" type="text/css">
 <img src="https://github.com/midiam1/Webmin/blob/main/img/webmin.png"/>
 # webmin<p><hr>
- Instalación de <strong><a href="https://www.webmin.com/" title="Ir a ..." target="_blank">webmin</a></strong> en Ubuntu.
+ Instalación de <strong><a href="https://www.webmin.com/" title="Ir a ..." target="_blank">webmin</a></strong> en <span class="Naranja">Ubuntu 18</span>.
 <hr/>
 
 <strong>Webmin</strong> es una herramienta de configuración de sistemas accesible vía web para sistemas Unix, como GNU/Linux y OpenSolaris. Con él se pueden configurar aspectos internos de muchos sistemas libres, como el servidor web Apache, PHP, MySQL, DNS, Samba, DHCP, entre otros.

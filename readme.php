548848847de867a201fd07465790958a4e13fe4b
midiam1 <admhimmeros@gmail.com>
2021-12-23 16:38:07 -0400

    Algunas mejoras

--- readme.php
+++ readme.php
@@ -1,9 +1,11 @@
 
 
 
-<img src="https://github.com/midiam1/Webmin/blob/main/img/webmin.png"/>
-# webmin<p><hr>
- Instalación de <strong><a href="https://www.webmin.com/" title="Ir a ..." target="_blank">webmin</a></strong> en <span class="Naranja">Ubuntu 18</span>.
+<img src="https://github.com/midiam1/Webmin/blob/main/img/webmin.png" width="25%" height="25%"/>
+
+<h2>#webmin</h2><br>
+<hr/>
+ Instalación de <strong><a href="https://www.webmin.com/" title="Ir a ..." target="_blank">webmin</a></strong> en <strong>Ubuntu 18</strong>.
 <hr/>
 
 <strong>Webmin</strong> es una herramienta de configuración de sistemas accesible vía web para sistemas Unix, como GNU/Linux y OpenSolaris. Con él se pueden configurar aspectos internos de muchos sistemas libres, como el servidor web Apache, PHP, MySQL, DNS, Samba, DHCP, entre otros.
